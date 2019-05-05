4b2c577211611be5a939b26211467cacb0cf2348
Ryo HIGASHIGAWA <higasigawa@co-routine.com>
2019-05-05 18:32:43 +0900

    history & free

--- step0_repl.c
+++ step0_repl.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
+char *READ(char prompt[]) {
+  char *line;
+  line = readline(prompt);
+
+  if (!line)
+    return NULL;
+
+  add_history(line);
+  return line;
+}
+
+char *EVAL(char *line) {
+  return line;
+}
+
+char *PRINT(char *line) {
+  return line;
+}
 
 int main() {
-  char *line;
+  char *ast, *expr;
+  char prompt[100];
+
+  snprintf(prompt, sizeof(prompt), "user> ");
+
   while (1) {
-    line = readline("user> ");
-    if (!line) {
+    ast = READ(prompt);
+    if (!ast) {
       putchar('\n');
       break;
     }
+
+    expr = EVAL(ast);
+    puts(PRINT(expr));
+
+    free(ast);
   }
   return 0;
 }
